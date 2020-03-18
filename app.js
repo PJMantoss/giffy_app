95a5ccffd438e02e2f350cf78df78484e6b466d8
pjmantoss <joelptoss@gmail.com>
2020-03-18 11:10:43 +0100

    update app.js

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ $(document).ready(function(){
     let $searchInput = $("#search");
 
     $("form").on("submit", function(e){
+
+        e.preventDefault();
+        
         let searchTerm = $searchInput.val();
         $searchInput.val("");
 
-        $.get()
+        $.get(
+            "http://api.giphy.com/v1/gifs/search",
+            {}
+        )
     })
 })
