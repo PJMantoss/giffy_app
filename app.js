149c2553545567e4e870fd3b5968cba0926252e3
pjmantoss <joelptoss@gmail.com>
2020-03-18 11:08:06 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     let $gifArea = $("#gif-area");
     let $searchInput = $("#search");
+
+    $("form").on("submit", function(e){
+        let searchTerm = $searchInput.val();
+    })
 })
